8082bbc1e56ea6e1de22495e8604d443dad4befa
Rayan <rayantjedo@gmail.com>
2025-09-25 09:35:38 +0200

    Readme update

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9851b5297dd0c2fc866af18f767c34a0fee208cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b149d75d415d5604996deff09a46bdc7f83bd18e")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
