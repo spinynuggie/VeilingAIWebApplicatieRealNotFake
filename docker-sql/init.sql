52f12c19a228f0cfcc0884c09141d63e0f77ab43
je-bolle-vader <ingmarjanssen@hotmail.com>
2025-10-07 15:15:04 +0200

    Docker en backend MySQL connectie toegevoegd

---
+++ docker-sql/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS test_table (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(50)
+);
+
+INSERT INTO test_table (name) VALUES ('Copilot'), ('Veiling'), ('Test'), ('meow');
