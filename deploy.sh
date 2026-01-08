01f26eb665e97af884573a055b249be1a1a921d0
Rayan <rayantjedo@gmail.com>
2026-01-08 12:35:50 +0100

    changed versiontag

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # Configuration - EDIT THESE
 DOCKER_USERNAME="rayantjedo"
 VERCEL_URL="veiling-ai-web-applicatie-real-not.vercel.app"
-TAG="v5-cookie-fix" # SameSite=None + ForwardedProto
+TAG="v8-maybe-final-ihope-csrffix" # SameSite=None + ForwardedProto
 
 echo "🚀 Starting Magic Deployment (Tag: $TAG)..."
 
