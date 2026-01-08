94f0b37e07fc06e0c785ae2204a6d3b7bd31a721
guusd <23118741@student.hhs.nl>
2026-01-08 10:15:18 +0100

    weer terug veranderd

--- frontend/app/(Algemeen)/landing/page.tsx
+++ frontend/app/(Algemeen)/landing/page.tsx
@@ -16,7 +16,7 @@ return (
       <h1 className="mt-8 text-8xl font-bold leading-tight">
         De grootste<br />
         internationale<br />
-        sierteeltmarktplaatsbooooooooooep
+        sierteeltmarktplaats
       </h1>
     </main>
   </Background>
